16eb41bd001f856caf9669597963604bf053db12
pattsai-wsu <patrick.tsai@wsu.edu>
2022-01-23 23:06:53 -0800

    Setting up mockito and updating dependencies

--- src/test/java/edu/wsu/backendapi/controller/TemplateControllerTest.java
+++ src/test/java/edu/wsu/backendapi/controller/TemplateControllerTest.java
@@ -3,8 +3,12 @@ package edu.wsu.backendapi.controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 public class TemplateControllerTest {
 
     private TemplateController testGetAll;
