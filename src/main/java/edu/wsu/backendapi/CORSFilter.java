18d319238f25fabc605f12cad9a1f64aedc9a423
pattsai-wsu <patrick.tsai@wsu.edu>
2022-02-08 13:59:05 -0800

    added CORS filter

--- src/main/java/edu/wsu/backendapi/CORSFilter.java
+++ src/main/java/edu/wsu/backendapi/CORSFilter.java
@@ -15,7 +15,7 @@ public class CORSFilter implements ContainerResponseFilter {
     public void filter(final ContainerRequestContext requestContext,
                        final ContainerResponseContext cres) throws IOException {
         cres.getHeaders().add("Access-Control-Allow-Origin", "*");
-        cres.getHeaders().add("Access-Control-Allow-Headers", "Content-Type, x-requested-with");
+        cres.getHeaders().add("Access-Control-Allow-Headers", "Content-Type, x-requested-with, siteflow-organization");
         cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     }
 }
